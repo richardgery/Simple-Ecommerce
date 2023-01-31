11f3addf71b4f0015b09010403cc04c2b225b99b
kancilcakep99 <kancilcakep99@gmail.com>
2023-01-31 18:31:18 +0700

    feat: Add Product to Cart and Add Remove Button Product from Cart in JS

--- js/ShoppingCart.js
+++ js/ShoppingCart.js
@@ -24,6 +24,153 @@ cartNotifContent.addEventListener("click", (event) => {
 });
 
 // add products to cart
+const addCart = document.getElementsByClassName("add-cart");
+const productRow = document.getElementsByClassName("product-row");
+
+for (var i = 0; i < addCart.length; i++) {
+  button = addCart[i];
+  button.addEventListener("click", addCartClicked);
+}
+
+function addCartClicked(event) {
+  button = event.target;
+  var cartItem = button.parentElement;
+  var imageSrc = cartItem.getElementsByClassName("image")[0].src;
+  var price = cartItem.getElementsByClassName("price")[0].innerText;
+
+  addItemToCart(price, imageSrc);
+  updateCartPrice();
+}
+
+function addItemToCart(price, imageSrc) {
+  var productRow = document.createElement("div");
+  productRow.classList.add("product-row");
+  var productsRow = document.getElementsByClassName("products-row")[0];
+  var cartImage = document.getElementsByClassName("cart-image");
+
+  for (var i = 0; i < cartImage.length; i++) {
+    if (cartImage[i].src == imageSrc) {
+      alert("This item has already been added to the cart");
+      return;
+    }
+  }
+
+  var cartRowItems = `
+    <div class="product-row">
+      <img class="cart-image" src="${imageSrc}">
+      <span class ="cart-price">${price}</span>
+      <input class="product-quantity" type="number" value="1">
+      <button class="remove-btn">Remove</button>
+    </div>
+      `;
+  productRow.innerHTML = cartRowItems;
+  productsRow.append(productRow);
+  productRow
+    .getElementsByClassName("remove-btn")[0]
+    .addEventListener("click", removeItem);
+  productRow
+    .getElementsByClassName("product-quantity")[0]
+    .addEventListener("change", changeQuantity);
+  updateCartPrice();
+}
+
+// Remove products from cart
+const removeBtn = document.getElementsByClassName("remove-btn");
+for (var i = 0; i < removeBtn.length; i++) {
+  button = removeBtn[i];
+  button.addEventListener("click", removeItem);
+}
+
+function removeItem(event) {
+  btnClicked = event.target;
+  btnClicked.parentElement.remove();
+  updateCartPrice();
+}
+
+// update quantity input
+// var quantityInput = document.getElementsByClassName('product-quantity')[0];
+
+// for (var i = 0; i < quantityInput; i++){
+//   input = quantityInput[i]
+//   input.addEventListener('change', changeQuantity)
+// }
+
+// function changeQuantity(event) {
+//   var input = event.target
+//   if (isNaN(input.value) || input.value <= 0){
+//     input.value = 1
+//   }
+//   updateCartPrice()
+// }
+// end of update quantity input
+
+// update total price
+// function updateCartPrice() {
+//   var total = 0
+//   for (var i = 0; i < productRow.length; i += 2) {
+//     cartRow = productRow[i]
+//   var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+//   var quantityElement = cartRow.getElementsByClassName('product-quantity')[0]
+//   var price = parseFloat(priceElement.innerText.replace('$', ''))
+//   var quantity = quantityElement.value
+//   total = total + (price * quantity )
+
+//   }
+//   document.getElementsByClassName('total-price')[0].innerText =  '$' + total
+
+// document.getElementsByClassName('cart-quantity')[0].textContent = i /= 2
+// }
+// end of update total price
+
+// purchase items
+// const purchaseBtn = document.querySelector('.purchase-btn');
+
+// const closeCartModal = document.querySelector('.cart-modal');
+
+// purchaseBtn.addEventListener('click', purchaseBtnClicked)
+
+// function purchaseBtnClicked () {
+//   alert ('Thank you for your purchase');
+//   cartModalOverlay.style.transform= 'translateX(-100%)'
+//  var cartItems = document.getElementsByClassName('product-rows')[0]
+//  while (cartItems.hasChildNodes()) {
+//    cartItems.removeChild(cartItems.firstChild)
+
+//  }
+//   updateCartPrice()
+// }
+// end of purchase items
+
+//alert user if cart is empty
+
+// open cart modal
+// const cart = document.querySelector('#cart');
+// const cartModalOverlay = document.querySelector('.cart-modal-overlay');
+
+// cart.addEventListener('click', () => {
+//   if (cartModalOverlay.style.transform === 'translateX(-200%)'){
+//     cartModalOverlay.style.transform = 'translateX(0)';
+//   } else {
+//     cartModalOverlay.style.transform = 'translateX(-200%)';
+//   }
+// })
+// end of open cart modal
+
+// close cart modal
+// const closeBtn = document.querySelector ('#close-btn');
+
+// closeBtn.addEventListener('click', () => {
+//   cartModalOverlay.style.transform = 'translateX(-200%)';
+// });
+
+// cartModalOverlay.addEventListener('click', (e) => {
+//   if (e.target.classList.contains('cart-modal-overlay')){
+//     cartModalOverlay.style.transform = 'translateX(-200%)'
+//   }
+// })
+// end of close cart modal
+
+// add products to cart
 // const addToCart = document.getElementsByClassName('add-to-cart');
 // const productRow = document.getElementsByClassName('product-row');
 
